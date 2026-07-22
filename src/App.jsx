0d27bb05025db9c65e31dadbca57d2bde3ad573c
Emmanuel Sierra Holguin <elperroc596@gmail.com>
2026-07-22 16:56:10 -0500

    Personalización del componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 export default function App() {
-const fecha = new Date().toLocaleString();
-return (
+    const fecha = new Date().toLocaleString();
+        return (
  <main>
  <h1>Hola SENA</h1>
  <p>soy emmanuel sierra holguin y esta es mi primera clase de Reactjs</p>
+ <p>espero aprender todo sobre reacjs y tambien de githud y vite.</p>
  </main>
 )
 }
