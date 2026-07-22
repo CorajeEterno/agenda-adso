a44184ec929122d86a4c357e99be8ca098c39fe6
Emmanuel Sierra Holguin <elperroc596@gmail.com>
2026-07-22 16:37:26 -0500

    Versión inicial - Componente Hola SENA

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+ <main>
+ <h1>Hola SENA</h1>
+ <p>soy emmanuel sierra holguin y esta es mi primera clase de Reactjs</p>
+ </main>
+)
+}
