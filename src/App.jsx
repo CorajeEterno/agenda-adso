2a621f1a69d7fcef80b6513caf748c6c203d6111
Emmanuel Sierra Holguin <elperroc596@gmail.com>
2026-08-05 17:47:03 -0500

    dentro de la carpeta src se creo la carpeta components, dentro de esta carpeta se creo el archivo ContactoCard.jsx, se actualizaron dentro de la carpeta src los archivos App.css,App.jsx,index.css,main.jsx y index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,63 @@
-export default function App() {
-    const fecha = new Date().toLocaleString();
-        return (
- <main>
- <h1>Hola SENA</h1>
- <p>soy emmanuel sierra holguin y esta es mi primera clase de Reactjs</p>
- <p>espero aprender todo sobre reacjs y tambien de githud y vite.</p>
- </main>
-)
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+
+export default function app(){
+    const contactos = [
+        {
+            id: 1,
+            nombre: "Carolina Pérez",
+            telefono: "300 123 4567",
+            correo: "carolina@sena.edu.co",
+            etiqueta: "Compañera"
+        },
+        {
+            id: 2,
+            nombre: "Juan Díaz",
+            telefono: "301 987 6543",
+            correo: "juan@sena.edu.co",
+            etiqueta: "Instructor",
+        },
+        {
+            id: 3,
+            nombre: "Luisa Martínez",
+            telefono: "320 555 7788",
+            correo: "luisa@sena.edu.co",
+            etiqueta: "Cliente",
+        },
+        {
+            id: 4,
+            nombre: "Yoiner Velez",
+            telefono: "300 678 9857",
+            correo: "YoinerVelez@sena.edu.co",
+            etiqueta: "Amigo",
+        },
+        {
+            id: 5,
+            nombre: "Diana Holguin",
+            telefono: "330 446 7789",
+            correo: "DianaH@gmail.com",
+            etiqueta: "Familia",
+        }
+    ];
+
+    return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO 📒</h1>
+
+      <p className="app-subtitle">Contactos guardados</p>
+
+      {contactos.map((c) => (
+        <ContactoCard
+          key={c.id}            
+          nombre={c.nombre}     
+          telefono={c.telefono} 
+          correo={c.correo}     
+          etiqueta={c.etiqueta} 
+        />
+      ))}
+
+      <p className="app-nota">
+      </p>
+    </main>
+    );
 }
---
+++ src/components/ContactoCard.jsx
@@ -0,0 +1,14 @@
+export default function ContactoCard({nombre,telefono,correo,etiqueta}){
+    return(
+        <div className="card-contacto">
+            <h3 className="card-nombre">{nombre}</h3>
+
+            <p className="card-linea">📞{telefono}</p>
+
+            <p className="card-linea">📧{correo}</p>
+            
+             <p className="card-etiqueta">{etiqueta}</p>
+            
+        </div>
+    );
+}
